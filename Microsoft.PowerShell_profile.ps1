40bc4451ff180837e6c247618344e18f7fafceaf
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2024-02-15 18:49:24 +0600

    refactor: added comments

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,12 @@
+# Autocomplete
 Import-Module PSReadLine
 Enable-PowerType
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
+
+# Icons for Get-ChildItem and it's aliases (dir or ls)
 Import-Module -Name Terminal-Icons
+
+
+# Initiate starship prompt
 Invoke-Expression (&starship init powershell)
