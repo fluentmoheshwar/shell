2eebac9847bd1f71205b37ef1ab46a38abd834a8
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2025-03-15 20:06:23 +0600

    feat: add aliases for windows PowerShell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,3 +10,10 @@ Import-Module -Name Terminal-Icons
 
 # Initiate starship prompt
 Invoke-Expression (&starship init powershell)
+
+# Aliases (for windows)
+# Set-Alias which where.exe
+# Set-Alias bash "C:\Program Files\Git\bin\bash.exe"
+# Set-Alias nano "C:\Program Files\Git\usr\bin\nano.exe"
+# Set-Alias dos2unix "C:\Program Files\Git\usr\bin\dos2unix.exe"
+# Set-Alias unix2dos "C:\Program Files\Git\usr\bin\unix2dos.exe"
